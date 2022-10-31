89b550415c454357205936991458e6c1b0b7fc68
Daniel Kuznetsov <danilgkuznet@icloud.com>
2022-10-31 00:13:12 -0700

    CRA isntallation, conventions

---
+++ Section 4/test-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Dog from "./Dog"
+
+function App() {
+  return (
+    <div className="App">
+      <Dog />
+    </div>
+  );
+}
+
+export default App;
---
+++ Section 4/test-app/src/Dog.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import './Dog.css';
+
+class Dog extends Component {
+    render() {
+        return (
+            <div className="Dog">
+                <h1>Dog!</h1>
+            </div>
+        )
+    }
+}
+
+export default Dog;
