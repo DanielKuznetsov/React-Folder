8eba6a9000c39dc2b379b3704050fc98c2231472
Daniel Kuznetsov <danilgkuznet@icloud.com>
2022-10-30 19:55:51 -0700

    Standard React App Layout/Structure

--- Section 2/index.js
+++ Section 2/index.js
@@ -14,36 +14,16 @@
 //     }
 // }
 
-const getNum = () => {
-    return Math.floor(Math.random() * 10) + 1;
-}
-
-class NumPicker extends React.Component {
+class App extends React.Component {
     render() {
-        const num = getNum();
-        let msg;
-        if(num === 7) {
-            msg = 
-                <div>
-                    <h2>Congrats! You win!</h2>
-                    <img src="https://i/giphy.com/media/nXx0jZrbnbRxS/giphy.webp" />
-                </div>;
-        } else {
-            msg = <p>Sorry, you lose!</p>
-        }
-
         return (
             <div>
-                <h1>Your number is: {num}</h1>
-                {msg}
-
-                {/* <p>{num === 7 ? "You got it!" : "Lost!"}</p>
-
-                {/* Short circuting */}
-                {/* {num === 7 && <img src="https://i/giphy.com/media/nXx0jZrbnbRxS/giphy.webp" />} */}
+                <NumPicker />
+                <NumPicker />
+                <NumPicker />
             </div>
         )
     }
 }
 
-ReactDOM.render(<NumPicker />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
