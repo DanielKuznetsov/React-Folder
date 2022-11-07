7465a4368d689323dc54f2afca16d031c69a1f07
Daniel Kuznetsov <danilgkuznet@icloud.com>
2022-11-06 19:23:27 -0800

    Added styling and winning logic

--- Section 11/lights-out/src/Board.js
+++ Section 11/lights-out/src/Board.js
@@ -1,7 +1,6 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Cell from "./Cell";
-import './Board.css';
-
+import "./Board.css";
 
 /** Game board of Lights out.
  *
@@ -33,40 +32,37 @@ class Board extends Component {
   static defaultProps = {
     nrows: 5,
     ncols: 5,
-    chanceLightStartsOn: 0.25,
-  }
-
+    chanceLightStartsOn: 0.25
+  };
   constructor(props) {
     super(props);
+
+    // TODO: set initial state
     this.state = {
       hasWon: false,
       board: this.createBoard()
-    }
+    };
   }
 
   /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
-  
+
   createBoard() {
     let board = [];
-    
-    for(let y = 0; y < this.props.nrows; y++) {
+    // TODO: create array-of-arrays of true/false values
+    for (let y = 0; y < this.props.nrows; y++) {
       let row = [];
-
-      for(let x = 0; x < this.props.ncols; x++) {
-        row.push(Math.random() < this.props.chanceLightStartsOn)
+      for (let x = 0; x < this.props.ncols; x++) {
+        row.push(Math.random() < this.props.chanceLightStartsOn);
       }
-
-      board.push(row)
+      board.push(row);
     }
-
     return board;
   }
 
   /** handle changing a cell: update board & determine if winner */
 
   flipCellsAround(coord) {
-    console.log("FLIPPING!")
-    let {ncols, nrows} = this.props;
+    let { ncols, nrows } = this.props;
     let board = this.state.board;
     let [y, x] = coord.split("-").map(Number);
 
@@ -77,43 +73,63 @@ class Board extends Component {
         board[y][x] = !board[y][x];
       }
     }
+    // TODO: flip this cell and the cells around it
+    flipCell(y, x); //Flip initial cell
+    flipCell(y, x - 1); //flip left
+    flipCell(y, x + 1); //flip right
+    flipCell(y - 1, x); //flip below
+    flipCell(y + 1, x); //flip above
 
-    // Flip initial cell
-    flipCell(y, x);
-    flipCell(y, x - 1);
-    flipCell(y, x + 1);
-    flipCell(y - 1, x);
-    flipCell(y + 1, x);
+    // win when every cell is turned off
+    // TODO: determine is the game has been won
+    let hasWon = board.every(row => row.every(cell => !cell));
 
-    let hasWon = false;
-
-    this.setState({board, hasWon});
+    this.setState({ board: board, hasWon: hasWon });
   }
 
-
   /** Render game board or winning message. */
-
-  render() {
+  makeTable() {
     let tblBoard = [];
-
-    for(let y = 0; y < this.props.nrows; y++) {
+    for (let y = 0; y < this.props.nrows; y++) {
       let row = [];
-      for(let x = 0; x < this.props.ncols; x++) {
-        let coord = `${y}-${x}`
-        row.push(<Cell key={coord} isLit={this.state.board[y][x]}
-                  flipCellsAroundMe={() => this.flipCellsAround(coord)}/>)
+      for (let x = 0; x < this.props.ncols; x++) {
+        let coord = `${y}-${x}`;
+        row.push(
+          <Cell
+            key={coord}
+            isLit={this.state.board[y][x]}
+            flipCellsAroundMe={() => this.flipCellsAround(coord)}
+          />
+        );
       }
-
-      tblBoard.push(<tr key={y}>{row}</tr>)
+      tblBoard.push(<tr key={y}>{row}</tr>);
     }
-
     return (
-      <table className="Board">
-        <tbody>
-          {tblBoard}
-        </tbody>
+      <table className='Board'>
+        <tbody>{tblBoard}</tbody>
       </table>
-    )
+    );
+  }
+  
+  render() {
+    return (
+      <div>
+        {this.state.hasWon ? (
+          <div className='winner'>
+            <span className='neon-orange'>YOU</span>
+            <span className='neon-blue'>WIN!</span>
+          </div>
+        ) : (
+          <div>
+            <div className='Board-title'>
+              <div className='neon-orange'>Lights</div>
+              <div className='neon-blue'>Out</div>
+            </div>
+            {this.makeTable()}
+          </div>
+        )}
+      </div>
+    );
   }
 }
 
