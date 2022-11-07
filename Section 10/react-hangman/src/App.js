d05cc0b365a93529be37b0879f73f91e98e88704
Daniel Kuznetsov <danilgkuznet@icloud.com>
2022-11-06 17:35:51 -0800

    Added keys, dynamic alt text to imgs, text for when the game is over, asnwer word to come up randomly, button to restart the game, and tracked incorrect guessed

--- Section 10/react-hangman/src/App.js
+++ Section 10/react-hangman/src/App.js
@@ -13,3 +13,4 @@ class App extends Component {
 }
 
 export default App;
+
--- Section 10/react-hangman/src/Hangman.js
+++ Section 10/react-hangman/src/Hangman.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./Hangman.css";
+import { randomWord } from "./words";
 import img0 from "./0.jpg";
 import img1 from "./1.jpg";
 import img2 from "./2.jpg";
@@ -17,8 +18,9 @@ class Hangman extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { nWrong: 0, guessed: new Set(), answer: "apple" };
+    this.state = { nWrong: 0, guessed: new Set(), answer: randomWord() };
     this.handleGuess = this.handleGuess.bind(this);
+    this.reset = this.reset.bind(this);
   }
 
   /** guessedWord: show current-state of word:
@@ -46,6 +48,7 @@ class Hangman extends Component {
   generateButtons() {
     return "abcdefghijklmnopqrstuvwxyz".split("").map(ltr => (
       <button
+        key={ltr}
         value={ltr}
         onClick={this.handleGuess}
         disabled={this.state.guessed.has(ltr)}
@@ -55,17 +58,31 @@ class Hangman extends Component {
     ));
   }
 
+  reset() {
+    this.setState({
+      nWrong: 0,
+      guessed: new Set(),
+      answer: randomWord(),
+    })
+  }
+
   /** render: render game */
   render() {
     return (
       <div className='Hangman'>
         <h1>Hangman</h1>
-        <img src={this.props.images[this.state.nWrong]} />
+        <img src={this.props.images[this.state.nWrong]} alt={`${this.state.nWrong}/${this.props.maxWrong}`} />
+        <p>Number wrong: {this.state.nWrong}</p>
         <p className='Hangman-word'>{this.guessedWord()}</p>
-        <p className='Hangman-btns'>{this.generateButtons()}</p>
+        {this.state.nWrong < this.props.maxWrong
+          ? <p className='Hangman-btns'>{this.generateButtons()}</p>
+          : <p>The game is over. The correct word is: <b>{this.state.answer}</b></p>
+        }
+        <button onClick={this.reset}>Restart the game!</button>
       </div>
     );
   }
 }
 
 export default Hangman;
+
