1ccd6da09d9a733fbd13211d349f54e77dffc042
Daniel Kuznetsov <danilgkuznet@icloud.com>
2022-11-06 16:47:46 -0800

    Refactored app to remove state from the child component and moved it into the parent

--- Section 9/color-boxes/src/Box.js
+++ Section 9/color-boxes/src/Box.js
@@ -4,21 +4,16 @@ import "./Box.css"
 class Box extends Component {
     constructor(props) {
         super(props)
-        this.state = {
-            bgColor: this.props.bgColor,
-        }
         this.handleBoxClick = this.handleBoxClick.bind(this);
     }
 
-    randomColor = () => Math.floor(Math.random()*16777215).toString(16);
-
     handleBoxClick() {
-        this.setState(curState => ({bgColor: curState.bgColor = `#${this.randomColor()}`}))
+        this.props.pick(this.props.value);
     }
 
     render() {
         return (
-            <button onClick={this.handleBoxClick} style={{backgroundColor: this.state.bgColor}} className='Box'></button>
+            <button onClick={this.handleBoxClick} style={{backgroundColor: this.props.bgColor}} className='Box'></button>
         )
     }
 }
--- Section 9/color-boxes/src/BoxWrapper.js
+++ Section 9/color-boxes/src/BoxWrapper.js
@@ -10,16 +10,24 @@ class BoxWrapper extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            numBox: Array.from({length: this.props.boxes}),
+            numBox: Array.from({length: this.props.boxes}, el => `#${this.randomColor()}`),
         }
+        this.pickColor = this.pickColor.bind(this);
     }
 
     randomColor = () => Math.floor(Math.random()*16777215).toString(16);
 
+    pickColor(index) {
+        this.setState(st => ({numBox: st.numBox.map(col => col === this.state.numBox[index] 
+            ? col = `#${this.randomColor()}` 
+            : col)}))
+    }
+
     render() {
         return (
             <div>
-                {this.state.numBox.map((el, i) => <Box key={i} bgColor={`#${this.randomColor()}`} />)}
+                {this.state.numBox.map((el, i) => 
+                    <Box key={i} value={i} bgColor={this.state.numBox[i]} pick={this.pickColor} />)}
             </div>
         )
     }
