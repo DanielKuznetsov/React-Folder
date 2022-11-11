093058e09ef2a24e1364cf908db3d13205a119bd
Daniel Kuznetsov <danilgkuznet@icloud.com>
2022-11-11 13:19:32 -0800

    Added loader for the fetching pause

--- Section 13/r-lifecycle/src/ZenQuote.js
+++ Section 13/r-lifecycle/src/ZenQuote.js
@@ -1,25 +1,44 @@
 import React, {Component} from "react";
-import axios from 'axios'
+// import axios from 'axios'
+import "./ZenQuote.css"
 
 class ZenQuote extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            quote: "",
+            rNum: "",
+            isLoaded: false,
         }
     }
-    
+
+    // This component triggers just once and is needed to start some action forever until we refresh the pafe or render a different component
     componentDidMount() {
-        axios.get("https://api.github.com/zen").then(response => {
-            this.setState({quote: response.data})
-        })
+        // axios.get("https://api.github.com/zen").then(response => {
+        //     setTimeout(
+        //         function() {
+        //             this.setState({quote: response.data})
+        //         }.bind(this), 3000
+        //     )
+        // }).catch(err => console.log(err))
+
+        setTimeout(
+            function() {
+                this.setState({rNum: Math.floor(Math.random() * 50) + 1, isLoaded: true})
+
+            }.bind(this), 3000
+        )
     }
 
     render() {
+
+
         return (
             <div>
-                <h1>Always remember...</h1>
-                <p>{this.state.quote}</p>
+                {this.state.isLoaded ? (
+                    <h1>Always remember... {this.state.rNum}</h1>
+                ) : (
+                    <div className="loader"></div>
+                )}
             </div>
         )
     }
