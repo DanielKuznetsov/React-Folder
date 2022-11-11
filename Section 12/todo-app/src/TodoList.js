1fe48dcecbe814c93c5128db9e58c1b9a4953da4
Daniel Kuznetsov <danilgkuznet@icloud.com>
2022-11-11 11:31:27 -0800

    Implemented editing todos

--- Section 12/todo-app/src/TodoList.js
+++ Section 12/todo-app/src/TodoList.js
@@ -8,6 +8,7 @@ class TodoList extends Component {
         this.state = {todos: []};
         this.create = this.create.bind(this);
         this.remove = this.remove.bind(this);
+        this.update = this.update.bind(this);
     }
 
     create(newTodo) {
@@ -22,9 +23,29 @@ class TodoList extends Component {
         })
     }
 
+    update(id, updatedTask) {
+
+        console.log(updatedTask)
+        const updatedTodos = this.state.todos.map(todo => {
+            if(todo.id === id) {
+                return {...todo, task: updatedTask}
+            }
+
+            return todo;
+        })
+
+        this.setState({todos: updatedTodos})
+    }
+
     render() {
         const todos = this.state.todos.map(todo => {
-            return <Todo key={todo.id} id={todo.id} task={todo.task} removeTodo={this.remove}/>
+            return <Todo 
+                key={todo.id} 
+                id={todo.id} 
+                task={todo.task} 
+                removeTodo={this.remove} 
+                updateTodo={this.update}
+            />
         })
 
         return (
