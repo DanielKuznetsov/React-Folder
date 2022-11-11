6f10e46fbb2a0ed2696e143700b5cb1b84a6fc89
Daniel Kuznetsov <danilgkuznet@icloud.com>
2022-11-11 12:41:12 -0800

    Example of componentDidMount which gets called by react just one time as soon as the component renders

---
+++ Section 13/r-lifecycle/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import DidMount from './DidMount';
+
+function App() {
+  return (
+    <div className="App">
+      <DidMount />
+    </div>
+  );
+}
+
+export default App;
---
+++ Section 13/r-lifecycle/src/DidMount.js
@@ -0,0 +1,28 @@
+import React, {Component} from "react";
+
+class DidMount extends Component {
+    constructor(props) {
+    super(props);
+    this.state = {
+        time: new Date(),
+        }
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(() => {
+            this.setState({
+                time: new Date(),
+            })
+        }, 1000)
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.time.getSeconds()}</h1>
+            </div>
+        )
+    }
+}
+
+export default DidMount;
