2e13e23de7ffacd82c4d0203ca8909202ee401df
Daniel Kuznetsov <danilgkuznet@icloud.com>
2022-11-12 16:03:14 -0800

    fetched new jokes

---
+++ Section 14/jokes/src/Joke.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Joke extends Component {
+    render() {
+        return (
+            <div>JokeList!</div>
+        )
+    }
+}
+
+export default Joke;
---
+++ Section 14/jokes/src/JokeList.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+class JokeList extends Component {
+    static defaultProps = {
+        numJokesToGet: 10,
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            jokes: []
+        }
+    }
+
+    async componentDidMount() {
+        let jokes = [];
+
+        while(jokes.length <= this.props.numJokesToGet) {
+            let res = await axios.get("https://icanhazdadjoke.com/", {
+                headers: {
+                    "Accept": "application/json"
+                }
+            });
+
+            jokes.push(res.data.joke);
+        }
+
+        this.setState({ jokes: jokes });
+    }
+
+
+
+
+    render() {
+        return (
+            <div className='JokeList'>
+                <h1>Dad Jokes!</h1>
+                <div className='JokeList-jokes'>
+                    {this.state.jokes.map(joke => (
+                        <div>{joke}</div>
+                    ))}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default JokeList;
