b34f40e87fbac5239b19396b3bf62883352446d6
Daniel Kuznetsov <danilgkuznet@icloud.com>
2022-11-14 21:59:02 -0800

    Added more starter files and installed Sass

--- Section 16/admin/src/App.js
+++ Section 16/admin/src/App.js
@@ -1,7 +1,10 @@
+import Home from "./pages/home/Home";
+
 function App() {
   return (
     <div className="App">
-      <div>Hello App!</div>
+      {/* <div>Hello App!</div> */}
+      <Home />
     </div>
   );
 }
