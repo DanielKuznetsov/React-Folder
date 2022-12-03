27c6dbdf4f275897963ee3668ea9e9a1798f25f6
Daniel Kuznetsov <danilgkuznet@icloud.com>
2022-12-03 14:06:35 -0800

    Added routes for users, hotels, rooms, and auth pages using middleware

--- Section 16/booking/api/index.js
+++ Section 16/booking/api/index.js
@@ -1,11 +1,27 @@
 import dotenv from "dotenv";
 import express from "express"; // "type": "module", in package.json
 import mongoose from "mongoose";
+import authRoute from "./routes/auth.js"
+import usersRoute from "./routes/users.js"
+import hotelsRoute from "./routes/hotels.js"
+import roomsRoute from "./routes/rooms.js"
 
 dotenv.config({ path: "./config.env" });
-
 const app = express();
 
+
+app.get("/", (req, res) => {
+    res.send('hello first request')
+})
+
+//middleware
+app.use("/api/auth", authRoute)
+app.use("/api/users", usersRoute)
+app.use("/api/hotels", hotelsRoute)
+app.use("/api/rooms", roomsRoute)
+
+
+// Server configuration
 const connect = async () => {
   try {
     await mongoose.connect(process.env.DATABASE);
