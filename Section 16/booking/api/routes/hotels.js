0d199a06678a6af60909be59e4f8c7d16b910486
Daniel Kuznetsov <danilgkuznet@icloud.com>
2022-12-03 16:04:30 -0800

    Created a Hotel schema

--- Section 16/booking/api/routes/hotels.js
+++ Section 16/booking/api/routes/hotels.js
@@ -1,9 +1,21 @@
 import express from "express";
+import Hotel from "../models/Hotel.js";
+// const Hotel = require("../models/Hotel")
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello, this is hotels endpoint!");
-});
+// Create
+router.post('/', async (req, res) => {
+    const newHotel = new Hotel(req.body)
+
+
+    try {
+        const savedHotel = await newHotel.save();
+        res.status(200).json(savedHotel);
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
 
 export default router;
+
